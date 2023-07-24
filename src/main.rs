9ab233077e6d75d9f8c3b8a322a748104aa26a7e
Neil Jav <j.nmka.g@gmail.com>
2023-07-23 23:48:14 -0600

    fix: proxy problem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use gloo_net::http::Request;
 use player::{video::Video, video_details::VideoDetails};
 use yew::prelude::*;
 
@@ -18,13 +19,34 @@ fn app() -> Html {
             />
         }
     });
+
+    let videos = use_state(|| vec![]);
+    {
+        let videos = videos.clone();
+        use_effect_with_deps(
+            move |_| {
+                let videos = videos.clone();
+                wasm_bindgen_futures::spawn_local(async move {
+                    let fetched_videos: Vec<Video> = Request::get("/tutorial/data.json")
+                        .send()
+                        .await
+                        .unwrap()
+                        .json()
+                        .await
+                        .unwrap();
+                    videos.set(fetched_videos);
+                })
+            },
+            (),
+        );
+    }
     html! {
     <>
     <h1> {  "Guitar yoga website"} </h1>
     <div>
     <h3>{"Lessons to watch"}</h3>
     <VideosList
-        videos = {player::fixtures::get_videos()}
+        videos = {(*videos).clone()}
         on_click={on_video_select.clone()}
         />
     </div>
